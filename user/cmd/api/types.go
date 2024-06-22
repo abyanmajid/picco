8ba209540da1f61fe2c4ad280096ead60dbbf1c5
abyanmajid <abyan@abydyl.net>
2024-06-23 05:01:12 +1000

    chore: disband migration branch

--- user/cmd/api/types.go
+++ user/cmd/api/types.go
@@ -18,3 +18,8 @@ type CreateUserPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
